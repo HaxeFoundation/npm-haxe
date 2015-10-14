209fb1e2c44034def5176176eda5a2933167fb02
clemos <cl3mos@gmail.com>
2015-10-14 17:21:36 +0200

    add package env var to local env

--- env.js
+++ env.js
@@ -1,7 +1,14 @@
 
 var path = require('path');
 
-module.exports = {
-	'HAXELIB_PATH': path.join(__dirname, 'lib'),
-	'HAXE_STD_PATH': path.join(__dirname, 'current/std')
+var env = module.exports = {};
+
+// clone local env
+// FIXME: use object.assign ?
+var local = process.env;
+for( k in local ) {
+	env[k] = local[k];
 }
+
+env['HAXELIB_PATH'] = path.join(__dirname, 'lib');
+env['HAXE_STD_PATH'] = path.join(__dirname, 'current/std');
--- exec.js
+++ exec.js
@@ -4,8 +4,9 @@ var spawn = require('child_process').spawn;
 var args = process.argv.slice(2);
 
 module.exports = function(command) {
-	var env = require('./env.js');
 	
+	var env = require('./env.js');
+
 	// For Node 0.6 compatibility, pipe the streams manually, instead of using
 	// `{ stdio: 'inherit' }`.
 	var cp = spawn(command, args, {
